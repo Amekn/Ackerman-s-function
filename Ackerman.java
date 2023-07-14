05aa31f716c61d5f14e45fe03e7c0f108cba798b
AemonZhou <dadaogcd4@gmail.com>
2023-07-14 22:46:00 +1200

    Implmentation of the class file and static method.

--- Ackerman.java
+++ Ackerman.java
@@ -1,1 +1,16 @@
-
+public class Ackerman{
+    private Ackerman(){};
+    
+    public static long AFuncRecursive(long m,  long n){
+        if(m == 0) return n + 1;
+        if(m > 0 && n == 0) return AFuncRecursive(m - 1, 1);
+        if(m > 0 && n > 0) return AFuncRecursive(m - 1, AFuncRecursive(m, n - 1));
+        return 0;
+    }
+    
+    public static long AFunc(long m, long n){
+        if(m < 0 || n < 0) throw new IllegalArgumentException("m and n can only be positive integer.");
+        if(m > 10 || n > 10) throw new IllegalArgumentException("m and n can only be smaller than 10.");
+        return AFuncRecursive(m, n);
+    }
+}
